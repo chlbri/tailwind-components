426e9d8ca6eebcc971adeed845c8bbef8ce2478c
chlbri <51156116+chlbri@users.noreply.github.com>
2021-07-21 19:09:00 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+function About(): JSX.Element {
+  return <div className='m-3'>About</div>;
+}
+
+export default About;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // screens: {
+      //   '3xl': '2000px',
+      // },
+    },
+  },
+  variants: {
+    extend: {
+      // animation: ['group-hover', 'responsive', 'hover', 'focus'],
+    },
+  },
+  plugins: [],
+};
